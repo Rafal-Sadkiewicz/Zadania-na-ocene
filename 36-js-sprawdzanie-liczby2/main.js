b769527ec508c781ee2de06e217322a085c2e85f
rafal-sadkiewicz <rafal.sadkiewicz89@gmail.com>
2020-08-26 15:02:00 +0200

    36-js-sprawdzanie-liczby2

---
+++ 36-js-sprawdzanie-liczby2/main.js
@@ -0,0 +1,18 @@
+let intPar1 = 10;
+let intPar2 = 20;
+
+
+function comperator (intA, intB)
+{
+    if  (intA == 20 || intB == 20 || intA+intB == 20)
+    {
+        return true;
+    }
+
+    else 
+    {
+        return false;
+    }
+}
+
+console.log(comperator(intPar1,intPar2));
