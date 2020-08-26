d321a0ecfafa46c720f1e239eff01366d456596f
rafal-sadkiewicz <rafal.sadkiewicz89@gmail.com>
2020-08-26 14:53:20 +0200

    35-js-sprawdzenie-liczby-v1

---
+++ 35-js-sprawdzenie-liczby/main.js
@@ -0,0 +1,11 @@
+let intPar = 99;
+
+if (intPar > 99 && intPar < 201)
+{
+    console.log('Liczba znajduje się w przedziale');
+}
+
+else
+{
+    console.log('Liczba nie znajduje się w przedziale');
+}
