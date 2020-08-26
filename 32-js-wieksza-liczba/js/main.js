e0b4ae66557e8463aad334600aa579d92bcd1ecd
rafal-sadkiewicz <rafal.sadkiewicz89@gmail.com>
2020-08-26 13:49:02 +0200

    32-js-wieksza-liczba

---
+++ 32-js-wieksza-liczba/js/main.js
@@ -0,0 +1,17 @@
+var num1 = 2;
+var num2 = 5;
+
+
+
+function comperator (intA, intB)
+{
+    if (intA>intB)
+    {
+        return intA;
+    }
+
+    return intB;
+}
+
+console.log(comperator(num1,num2));
+
